0649d069aef401d084948f8c302d0c91d3ff79a1
Mike Burr <mburr@unintuitive.org>
2018-08-28 17:40:21 -0700

    multi-price analysis

--- base.py
+++ base.py
@@ -59,12 +59,14 @@ class PriceSeries(object):
             column_names=', '.join(column_names),
             q_marks=', '.join(['?']*len(schema))
         )
+
+    # todo. this needs to be calculated based on the path given by user
     _sqlite_db = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'db.sqlite3')
 
     _connection = None
 
     @classmethod
-    def connection(cls):
+    def connect(cls):
         """The sqlite connection is a class singleton.
 
         https://www.sqlite.org/faq.html#q5
@@ -82,11 +84,70 @@ class PriceSeries(object):
             atexit.register(_cleanup)
         return cls._connection
 
-    def __init__(self, symbol_id):
+    @classmethod
+    def validate_datetime_object(cls, dt):
+        assert dt.tzname() == 'UTC', 'tzname==`{}`. Expected `UTC`'.format(dt.tzname())
+        assert dt.hour in (0, 6, 12, 18)
+        assert not dt.hour % 6, 'hour==`{}` not a multiple of `6`'.format(dt.hour)
+        for attr in 'minute', 'second', 'microsecond':
+            value = getattr(dt, attr)
+            assert value == 0, 'datetime attribute `{}`==`{}`. Expected `0`'.format(attr, value)
+
+    @classmethod
+    def get_normalized_datetime(cls, dt):
+        if not isinstance(dt, datetime.datetime):
+            dt = parse_dt(dt)
+        return dt.strftime(cls.date_format_template)
+
+    @classmethod
+    def round_up(cls, dt):
+        hour_increment = 6 - dt.hour % 6
+        dt += datetime.timedelta(hours=hour_increment)
+        dt = dt.replace(minute=0, second=0, microsecond=0)
+        return dt
+
+    def __init__(self, symbol_id, create_store=True):
         logger.debug('creating PriceSeries object for {}'.format(symbol_id))
         self.symbol_id = symbol_id
-        self._create_store()
         self.row_template = self.row_template.format(symbol_id=self.symbol_id)
+        if create_store:
+            self._create_store()
+
+    def get_prices_since(self, start_dt):
+        """Get prices for this PriceSeries where datetime is greater or equal to `start_dt`
+        """
+        # Since sqlite does not have native date times, we get the rowid for the row containing
+        # date `start_dt` and then do a second SQL query for rows with that ID or greater.
+        try:
+            start_dt = parse_dt(start_dt)
+        except TypeError:
+            pass
+        start_dt = self.get_normalized_datetime(self.round_up(start_dt))
+        sql = 'SELECT _ROWID_ FROM {symbol_id} WHERE {TIME} = \'{start_dt}\' LIMIT 1;'.format(
+            symbol_id=self.symbol_id,
+            TIME=self.TIME,
+            start_dt=start_dt,
+        )
+        cursor = self.connect().cursor()
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        if len(results) != 1:
+            raise Exception('Failed to get exactl one row when executing "{}"'.format(sql))
+        columns, = results
+        rowid, = columns
+        sql = 'SELECT {TIME}, {PRICE} FROM {symbol_id} WHERE _ROWID_ >= {rowid};'.format(
+            TIME=self.TIME,
+            PRICE=self.PRICE,
+            symbol_id=self.symbol_id,
+            rowid=rowid,
+        )
+        cursor = self.connect().cursor()
+        cursor.execute(sql)
+        results = cursor.fetchall()
+
+        return results
+
+
 
     def get_url(self, query_data):
         url_1 = ('https', 'rest.coinapi.io/v1', 'ohlcv/{}/history'.format(self.symbol_id))
@@ -114,24 +175,11 @@ class PriceSeries(object):
         create_table.append(fields)
         create_table.append(');')
         create_table = '\n'.join(create_table)
-        cursor = self.connection().cursor()
+        cursor = self.connect().cursor()
         cursor.execute(create_table.format(
             symbol_id=self.symbol_id
         ))
-        self.connection().commit()
-
-    def get_normalized_datetime(self, dt):
-        if not isinstance(dt, datetime.datetime):
-            dt = parse_dt(dt)
-        return dt.strftime(self.date_format_template)
-
-    def validate_datetime_object(self, dt):
-        assert dt.tzname() == 'UTC', 'tzname==`{}`. Expected `UTC`'.format(dt.tzname())
-        assert dt.hour in (0, 6, 12, 18)
-        assert not dt.hour % 6, 'hour==`{}` not a multiple of `6`'.format(dt.hour)
-        for attr in 'minute', 'second', 'microsecond':
-            value = getattr(dt, attr)
-            assert value == 0, 'datetime attribute `{}`==`{}`. Expected `0`'.format(attr, value)
+        self.connect().commit()
 
     def fetch(self):
         last_date = self.get_last_date_from_store()
@@ -164,7 +212,7 @@ class PriceSeries(object):
             the_columns=', '.join((self.TIME, self.PRICE)),
             symbol_id=self.symbol_id,
         )
-        cursor = self.connection().cursor()
+        cursor = self.connect().cursor()
         cursor.execute(sql)
         results = cursor.fetchall()
         if len(results) == 0:
@@ -185,11 +233,11 @@ class PriceSeries(object):
             insert_rows.append(values)
         if insert_rows:
             try:
-                cursor = self.connection().cursor()
+                cursor = self.connect().cursor()
                 cursor.executemany(self.row_template, insert_rows)
             finally:
                 logger.debug('done inserting. committing...')
-                self.connection().commit()
+                self.connect().commit()
 
     def update(self):
         data = self.fetch()
