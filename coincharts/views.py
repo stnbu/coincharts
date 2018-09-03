448d8a8a7b6f06f9a39598747d79cecf8c77ebaf
Mike Burr <mburr@unintuitive.org>
2018-09-03 10:53:41 -0700

    Getting web stuff to work
    * relocating `index.html`
    * adding django's idea of a "minimal" `settings.py`
    * trivial fetching of data in `views.py`. works.
    * adding `wsgi.py`, also "required"

--- coincharts/views.py
+++ coincharts/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
-#from .models import Page, Content
+from .models import Prices
 
-def page(request, current_page_name):
+def index(request):
+    prices = Prices.objects.all()[:100]
     context = {
+        'prices': prices,
     }
-    return render(request, 'main/index.html', context)
+    return render(request, 'coincharts/index.html', context)
